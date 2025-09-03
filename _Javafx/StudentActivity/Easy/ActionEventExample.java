64e622a44ecd228f986a78dd20290e8e05ec541d
meetlittleinventors <meet@littleinventors.live>
2025-09-03 18:15:42 +0530

    Update and rename FXStudentEasy2.java to ActionEventExample.java

---
+++ _Javafx/StudentActivity/Easy/ActionEventExample.java
@@ -0,0 +1,26 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class ActionEventExample extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        Button button = new Button("Click Me!");
+        
+        // Use a lambda expression for event handling
+        button.setOnAction(event -> {
+            System.out.println("Button clicked!");
+        });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(button);
+        
+        Scene scene = new Scene(root, 300, 200);
+        primaryStage.setTitle("Action Event");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
